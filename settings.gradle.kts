4f44a925a4d53c7f28758fe687750f5545b0d495
guoqi <1395772260@qq.com>
2025-12-25 11:17:00 +0800

    版本运行

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,20 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/android/maven/google") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/android/maven/public") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/gradle-plugin") }
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
-        google()
-        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/android/maven/google") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/android/maven/public") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/gradle-plugin") }
     }
 }
 
